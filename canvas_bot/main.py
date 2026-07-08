44dc698af217dae3ef9f25ece03bafa92732ad4b
Oasis Pandey <fno21@txstate.edu>
2026-07-08 09:53:00 -0500

    Fix 501 Not Implemented error by adding do_HEAD to health check server

--- canvas_bot/main.py
+++ canvas_bot/main.py
@@ -563,6 +563,12 @@ if __name__ == "__main__":
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"OK")
+            
+        def do_HEAD(self):
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            
         def log_message(self, format, *args):
             pass # Keep logs clean from pings
 
